1189682c02aeff314d439e38a0250a54021b0dea
PedroHenriqueSPadilha <pedrohenriquespadilha@gmail.com>
2024-03-11 23:17:59 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2
+{
+    internal class Program
+    {
+        public struct FormasGeometricas
+        {
+            public double Base;
+            public double altura;
+        }
+        static void Gravar(ref FormasGeometricas FG)
+        {
+            Console.Write("Entre com a base do triangulo: ");
+            FG.Base = double.Parse(Console.ReadLine());
+            Console.Write("Entre com a altura do triangulo: ");
+            FG.altura = double.Parse(Console.ReadLine());
+        }
+        static double AreaTriangulo(ref FormasGeometricas FG)
+        {
+            double result = 0;
+            result = (FG.Base * FG.altura) / 2;
+            return result;
+        }
+
+        static void Mostrar(ref FormasGeometricas f)
+        {
+            Console.WriteLine($"O total: {AreaTriangulo(ref f)}");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            FormasGeometricas f1 = new FormasGeometricas();
+            Gravar (ref f1);
+            Mostrar(ref f1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AtividadePOO3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO3
+{
+    internal class Program
+    {
+        public struct Funcionario
+        {
+            public string nome;
+            public double salario;
+
+        }
+
+        static void Gravar(ref Funcionario F)
+        {
+            Console.Write("Digite o seu nome:");
+            F.nome = Console.ReadLine();
+            Console.Write("Digite o seu salário:");
+            F.salario = double.Parse(Console.ReadLine());
+        }
+
+        static void Mostrar(Funcionario F)
+        {
+            Console.WriteLine($"O funcionario: {F.nome}, recebe:{F.salario.ToString("0,00")}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            int op = 0;
+            Funcionario a1;
+            a1.nome = "";
+            a1.salario = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1-Gravar Nome e Salário");
+                Console.WriteLine("2-Mostrar Salário do Funcionario");
+                Console.WriteLine("3-Sair");
+                Console.Write("Entre com uma opção:");
+                op = int.Parse(Console.ReadLine());
+
+
+                switch (op)
+                {
+                    case 1:
+                        Gravar(ref a1);
+                        break;
+
+                    case 2:
+                        Mostrar(a1);
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (op != 3);
+        }
+    }
+}
